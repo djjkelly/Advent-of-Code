a720d379e633edb9462f54bd6b8bbee5d7529640
Duncan Kelly <djjkelly01@gmail.com>
2024-01-15 16:22:55 +0000

    Code repaired - correct answer obtained.
    The code has been corrected to:
    - find spelled numbers as well as digits
    - use a dict to record the positions of candidate numbers (digit and spelled)
    - only replace values in the dicts when the position is closer to the beginning or end of a line than previous candidate numbers

--- 2023/2023_Day1_part2.py
+++ 2023/2023_Day1_part2.py
@@ -10,26 +10,82 @@ num_dict = {
     'eight':'8',
     'nine':'9',
     }
+
 with open("2023/2023_Day1_input.txt") as file:
     total = 0
     for line in file:
-        numerical_line = line
-        for key,value in num_dict.items():
-            numerical_line = numerical_line.replace(key,value)
         print(line.strip())
-        print(numerical_line.strip())
-        for char in numerical_line:
-            if char.isnumeric():
-                print(char)
-                first_digit = char
+        position_dict = {
+            '1':None,
+            '2':None,
+            '3':None,
+            '4':None,
+            '5':None,
+            '6':None,
+            '7':None,
+            '8':None,
+            '9':None,
+            }
+        reversed_dict = position_dict.copy()
+        reversed_line = line[::-1]
+
+        # this finds the first numerical character in the string:
+        position=0
+        for char in line:
+            position+=1
+            if char.isnumeric() and position_dict[char]==None:
+                position_dict[char] = position-1
                 break
-        reversed_line = numerical_line[::-1]
-        print(reversed_line.strip())
+
+        # this finds the first number written in alphabetic characters:
+        for alphabetic_key,value in num_dict.items():
+            position = line.find(alphabetic_key)
+            key=num_dict[alphabetic_key]
+            if position > -1 and position_dict[key]==None:
+                position_dict[key] = position
+            elif position >-1 and position < position_dict[key]:
+                position_dict[key] = position            
+
+        # this states the earliest position of any number appearing in the string, written in either format
+        earliest_position = float('inf')
+        for key in position_dict:
+            value = position_dict[key]
+            if value == None:
+                continue
+            if int(value) < earliest_position:
+                earliest_position = int(value)
+                first_digit = key
+
+        # this finds the first numerical character in the REVERSED string
+        position=0
         for char in reversed_line:
-            if char.isnumeric():
-                print(char)
-                last_digit = char
+            position+=1
+            if char.isnumeric() and reversed_dict[char]==None:
+                reversed_dict[char] = position-1
                 break
-        total += int(first_digit+last_digit)
-        print(total)
+        #print(reversed_dict)
+
+        # this finds the first number written in alphabetic characters in the REVERSED STRING
+        for alphabetic_key,value in num_dict.items():
+            position = reversed_line.find(alphabetic_key[::-1])
+            key=num_dict[alphabetic_key]
+            if position > -1 and reversed_dict[key]==None:
+                reversed_dict[key] = position
+            elif position >-1 and position < reversed_dict[key]:
+                reversed_dict[key] = position
+        #print(reversed_dict)
+
+        # this states the earliest position of any number appearing in the string, written in either format
+        last_position = float('inf')
+        for key in reversed_dict:
+            value = reversed_dict[key]
+            if value == None:
+                continue
+            if int(value) < last_position:
+                last_position = int(value)
+                last_digit = key
+
+        subtotal = first_digit + last_digit
+        print(subtotal + '\n')
+        total += int(subtotal)
 print (total)
