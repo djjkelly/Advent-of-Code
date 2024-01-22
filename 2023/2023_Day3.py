2c86a6adde519b5574c688dcc0986f773dae7169
Duncan Kelly <djjkelly01@gmail.com>
2024-01-22 19:17:52 +0000

    This version attempts to solve the problem by

--- 2023/2023_Day3.py
+++ 2023/2023_Day3.py
@@ -10,92 +10,78 @@ except Exception as e:
     print(f"An error was encountered: {e}")
 
 symbols = "!@£$%^&*()-=_+|/\\#"
-previous_line = '............................................................................................................................................'
-'''Each symbol is surrounded by an imaginary "square" of eligibility.
-Within this square every digit found will be considered as an eligible digit in a part number.
-All digits sequentially adjacent to a digit in a part number eligible digit is included in the part number.
-'''
-total = 0
-for line_number, current_line in enumerate(file_content):
-    current_line = current_line.strip()
 
-    # previous_line = '.....654.'
-    # current_line = '855*.....'
+for line_number in range(len(file_content)):
 
-    print(previous_line)
-    print(current_line)
+    current_line = file_content[line_number].strip()
+    try:
+        next_line = file_content[line_number + 1].strip()
+    except:
+        next_line = ''
+        for character in range(len(current_line)):
+            next_line.append('.')
 
-    subtotal = 0
+    current_line = '...234...'
+    next_line    = '..!......'
 
-    current_number_string = '0'
-    previous_line_number_string = '0'
-    current_number_is_part_number = False
-    number_above_is_part_number = False
-    for character_number,character in enumerate(current_line):
-        character_above = previous_line[character_number]
-        # this section sets "is_part_number" to True if any of the relevant 2 or 3 characters on the line above are symbols.
-        if previous_line[character_number] in symbols:
-            current_number_is_part_number = True
-            number_above_is_part_number = True
+    line_total = 0
 
-        if character_number > 0 and previous_line[character_number-1] in symbols:
-            current_number_is_part_number = True
-            number_above_is_part_number = True
+    current_line_num = ''
+    next_line_num = ''
+    diagonal_down = '' # This is any numbers on the next_line affected by symbols on the current_line
+    diagonal_up = '' # This is any numbers on the current_line affected by symbols on the next_line
 
-        if character_number+1 < len(current_line):
-            if previous_line[character_number+1] in symbols:
-                current_number_is_part_number = True
-            number_above_is_part_number = True
+    current_is_part = False
+    next_is_part = False
 
-        # this section looks for numbers on the line above which may correspond to a symbol on the current line
-        if previous_line[character_number].isnumeric():
-            previous_line_number_string += previous_line[character_number]
-            print("Number found on previous line! Previous number updated to " + previous_line_number_string)
-        else:
-            previous_line_number_string = '0'
+    for char in range(len(current_line)):
+        if current_line[char].isnumeric():
+            current_line_num += current_line[char]
+            #print("current_line_num is " + current_line_num)
 
-        if character == "." or character_number==len(current_line)-1:
-            if current_number_is_part_number and current_number_string != '0':
-                print("Current number found. Adding " + current_number_string)
-                subtotal += int(current_number_string)
-            current_number_is_part_number = False
-            number_above_is_part_number = False # sceptical of this
+        if next_line[char].isnumeric():
+            next_line_num += next_line[char]
+            #print("next_line_num is " + next_line_num)
+        
+        # diagonal down and to right
+        if current_line[char] in symbols:
+            print("Symbol on current line: current_is_part = True")
+            current_is_part = True
+            i = 1
+            while next_line[char+i].isnumeric():
+                diagonal_down += next_line[char+i]
+                i += 1
+            print("Number found down and right from current line symbol, added to diagonal_down: " + diagonal_down)
+        
+        # diagonal up and to right
+        if next_line[char] in symbols:
+            print("Symbol on next line: next_is_part = True")
+            next_is_part = True
+            i = 1
+            while current_line[char+i].isnumeric():
+                diagonal_up += current_line[char+i]
+                i += 1
+            print("Number found up and right from next line symbol, added to diagonal_up: " + diagonal_up)
 
-            current_number_string = '0'
+        # This takes the num being stored for the CURRENT line and adds it to the line total
+        if current_line[char] == '.' or char == len(current_line):
+            if current_is_part and current_line_num != '':
+                print("Eligible number ended: Line total updated with current_line_num; current_line_num set to False")
+                line_total += current_line_num
+                current_is_part = False
+    print("End of the line!")
 
-            if number_above_is_part_number:
-                if previous_line[character_number+1].isnumeric():
-                    previous_line_number_string += previous_line[character_number+1]
-                    if previous_line[character_number+2].isnumeric():
-                        previous_line_number_string += previous_line[character_number+2]
-
-                print("Number above found. Adding " + previous_line_number_string)
-                subtotal += int(previous_line_number_string)
-                number_above_is_part_number = False
-                previous_line_number_string = '0'
     
-        elif character.isnumeric():
-            current_number_string+=character
-            print("Number found! Number updated to " + current_number_string)
-        elif character in symbols:
-            current_number_is_part_number = True
-            number_above_is_part_number = True
-        else:
-            print("ERROR: Something has gone wrong, character unaccounted for!")
-    if current_number_is_part_number:
-        print("Adding " + current_number_string)
-        subtotal += int(current_number_string)
-
-    # This line will need to be at the end of the character-wise for loop.
-    previous_line = current_line
-
-
-print("Total: " + str(total))
-
-'''
-Line 1 ("0") should have a total of 0
-Line 2 should add a total of ^0^ + 305 + 901 + 514 = 1720
-Line 3 should have a total of ^1720^ + 783 + 9 + 855 + 940 + 844 + 257 = 5408
-'''
-
-# remember to deal with the case where a number on the line above has already been counted.
+        
+        
+        
+        # if there is a number:
+            # get the whole number (go until a dot or the end of the line)
+            # scan for symbols (right, down, diagonal)
+        
+        # if there is a symbol:
+            # scan for numbers
+            # get the whole number (go until a dot or the end of the line)
+        #
+        #
+        #
