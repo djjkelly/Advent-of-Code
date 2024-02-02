e1c27db0f22f53d87ef12d36300bffa8f2a0e5a4
Duncan Kelly <djjkelly01@gmail.com>
2024-02-02 17:16:57 +0000

    2021 Day 2 Part 2 Complete

--- 2021/2021_Day2_Part2.py
+++ 2021/2021_Day2_Part2.py
@@ -1,20 +1,28 @@
 with open("2021/2021_Day2_input.txt") as file_object:
     file_content = file_object.readlines()
+
+'''
+Forward increases horizontal position * X AND increases depth by aim * X
+Down increases aim by X
+Up decreases aim by X
+'''
+aim = 0
 horizontal_position = 0
 depth = 0
 
-'''
-Forward increases horizontal position
-Down increases depth
-
-'''
 for line in file_content:
     if "forward" in line:
-        horizontal_position += int(line.split()[1])
-    if "down" in line:
-        depth += int(line.split()[1])
-    if "up" in line:
-        depth -= int(line.split()[1])
+        forward_number = int(line.split()[1])
+        horizontal_position += forward_number
+        depth += aim * forward_number
 
-answer = depth * horizontal_position
+    if "down" in line:
+        down_number = int(line.split()[1])
+        aim += down_number
+
+    if "up" in line:
+        up_number = int(line.split()[1])
+        aim -= up_number
+
+answer = horizontal_position * depth
 print(answer)
