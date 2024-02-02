45c862a5c19eddea34850b9c9a566e75c079d763
Duncan Kelly <djjkelly01@gmail.com>
2024-02-02 14:13:32 +0000

    Using a shorter test dataset to develop the code. Function called

--- 2023/2023_Day5_Part1.py
+++ 2023/2023_Day5_Part1.py
@@ -3,22 +3,33 @@
 
 import re
 try:
-    with open("2023/2023_Day5_input.txt") as file_object:
+    with open("2023/2023_Day5_testinput.txt") as file_object:
         file_content = file_object.readlines()
 
     data_dict = {}
     current_header = None
     current_lines = []
+    minimum_location_number = float('inf')
 
     def translate(current_number,key):
         #print(data_dict[key])
         for element in data_dict[key]:
-            destination_start = element["Destination start point"]
-            source_start = element["Source start point"]
-            range_length = element["Range length"]
-            destination_end = destination_start + range_length
-            source_end = source_start + range_length
-            print(f'Source range: {source_start} - {source_end}. Destination range: {destination_start} - {destination_end}')
+            result = None
+            destination_start = int(element["Destination start point"])
+            source_start = int(element["Source start point"])
+            range_length = int(element["Range length"])-1
+            source_end = int(source_start + range_length)
+            print(f'Source range: {source_start} - {source_end}.')
+            if current_number >= source_start and current_number <= source_end:
+                result = current_number - source_start + destination_start
+                print(f'The source range contains the current number! Returning {result}')
+                return(result)
+            else:
+                print(f'The number is not translated')
+                continue
+        if not result:
+            print(f"Returning current number {current_number}")
+            return(current_number)
 
     for line_number,line in enumerate(file_content):
         line = line.strip()
@@ -39,15 +50,17 @@ try:
     #print(data_dict)
     seeds = data_dict['seeds'][0].split()
     for seed_number in seeds:
-        #print("This is seed number: ",seed_number)
-        soil_number = translate(seed_number,"seed-to-soil map")
-        #soil_number = 1
-        fertiliser_number = 2
-        water_number = 3
-        light_number = 4
-        temperature_number = 5
-        humidity_number = 6
-        location_number = 7
+        print("This is seed number: ",seed_number)
+        soil_number = translate(int(seed_number),"seed-to-soil map")
+        fertiliser_number = translate(soil_number,"soil-to-fertilizer map")
+        water_number = translate(fertiliser_number,"fertilizer-to-water map")
+        light_number = translate(water_number,"water-to-light map")
+        temperature_number = translate(light_number,"light-to-temperature map")
+        humidity_number = translate(temperature_number,"temperature-to-humidity map")
+        location_number = translate(humidity_number,"humidity-to-location map")
+        if location_number < minimum_location_number:
+            minimum_location_number = location_number
+        print(f'The minimum location number is {minimum_location_number}')
 
 except FileNotFoundError:
     print("File not found")
