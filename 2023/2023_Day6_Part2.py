aff61ca985775caf221246734e633a53b2e24e55
Duncan Kelly <djjkelly01@gmail.com>
2024-03-26 16:24:46 +0000

    Correct answer found for part 2
    worth noting that the solution is a little slow and is not the most computationally efficient way of solving the problem.
    One or more of the following approaches could be taken next:
    - picking numbers in the range and iterating to find the minimum time which beats the record.
    - mirroring the range, as the curve for distance with time is symmetrical
    - starting the search in the middle of the range and working outward
    - once the minimum time to beat the record is established, calculate the number of speeds between that and the midpoint of the race_time. Double the even portion of that number.

--- 2023/2023_Day6_Part2.py
+++ 2023/2023_Day6_Part2.py
@@ -12,21 +12,12 @@ print("time input: ",time_input,"distance input: ",distance_input)
 #time_input,distance_input = test_time_input,test_distance_input
 #print("test time input: ",time_input,"test distance input: ",distance_input) # test should give rise to a product of 71503 as per example on website
 
-ways_to_win_list = []
-'''
-for race in range(len(time_input)):
-    ways_to_win = 0
-    race_time,distance_record = time_input[race],distance_input[race]
-    #print("Race ",race, ": race time: ",race_time,", distance record: ",distance_record)
-    for speed in range(race_time+1):
-        distance = speed * (race_time - speed)
-        #print("speed: ",speed,"distance: ",distance)
-        if distance > distance_record:
-            ways_to_win += 1
-    #print("ways to win :", ways_to_win)
-    ways_to_win_list.append(ways_to_win)
-product = 1
-for factor in ways_to_win_list:
-    product *= factor
-print(product)
-'''
+ways_to_win = 0
+race_time,distance_record = int(time_input),int(distance_input)
+#print("race time: ",race_time,", distance record: ",distance_record)
+for speed in range(race_time+1):
+    distance = speed * (race_time - speed)
+    #print("speed: ",speed,"distance: ",distance)
+    if distance > distance_record:
+        ways_to_win += 1
+print("ways to win :", ways_to_win) # correct answer found - 36872656. this is quite a slow method which takes about 15 seconds on my computer.
