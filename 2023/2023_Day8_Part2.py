8b08bc5529bc48477aed6d4398ded4766488126f
Duncan Kelly <djjkelly01@gmail.com>
2024-04-15 18:42:17 +0100

    Better list unpack & redundant function removed

--- 2023/2023_Day8_Part2.py
+++ 2023/2023_Day8_Part2.py
@@ -23,43 +23,28 @@ for line in file_content:
     first_line = False
 print(f'network_map loaded. Commencing while loop. start_locations: \n{start_locations}')
 
-# TEST - Correct answer should be 6
-
 def look_up_next_element(location,instruction):
     next_element = network_map[location][instruction]
     return next_element
-#print(f'lookup element test: ',look_up_next_element('DHD','L'))
 
-def initialise_findings_table(start_locations):
-    output = []
-    for index in range(len(start_locations)):
-        output.append([])
-    return output
-findings = initialise_findings_table(start_locations)
-
-count_array = []
+count_list = []
 locations = start_locations
 for location_number in range(len(locations)):
     answer_found = False
     count = 0
     location = locations[location_number]
-    #print('C ommencing search from new starting location:', location,'. Location number: ', location_number)
     while answer_found is False:
         for instruction in instructions:
-            #print('new instruction: ', instruction)
             location = look_up_next_element(location,instruction)
-            #print(f'next location found: {location}')
             count += 1
             if location[2] == 'Z':
                 answer_found = True
                 break
     print('Answer found! location ',locations[location_number],'number of moves: ',count)
-    count_array.append(count)
-    print(count_array)
+    count_list.append(count)
+print(count_list)
 
-for element in count_array:
-    print(element)
-    final_count = math.lcm(int(element),final_count)
+final_count = math.lcm(*tuple(count_list))
 
 print('Final count: ',final_count)
 '''
