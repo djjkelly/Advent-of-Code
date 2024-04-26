56b2e3d39323fe45ecf0d222e369d7c8ef9fa802
Duncan Kelly <djjkelly01@gmail.com>
2024-04-26 14:58:13 +0100

    Strings of 1s and 0s to represent combinations
    My latest progress provides a representations of all possible combinations for the uncertain springs.
    The number of combinations matches the prediction of the math.comb() (or "nCr") formula.
    Now I need to take those strings and check them.
    I think I'll do this by translating each string into the structure of the damaged_group_sizes.
    If the output is the same, it is a viable permutation and the count can increase.

--- 2023/2023_Day12_Part1.py
+++ 2023/2023_Day12_Part1.py
@@ -2,6 +2,7 @@
 #https://adventofcode.com/2023/day/12
 
 import math
+from itertools import product
 
 with open("2023/2023_Day12_testinput.txt") as file_object:
     file_content = file_object.readlines()
@@ -14,9 +15,27 @@ for line in file_content:
     damaged_group_sizes = damaged_group_sizes.split(',')
     content_list.append([condition_records,damaged_group_sizes])
 
+def generate_combinations(uncertain_springs):
+    all_combinations = [''.join(seq) for seq in product('01',repeat=uncertain_springs)]
+    return all_combinations
+
+def remove_wrong_numbers(test_list,number_expected):
+    new_test_list = test_list.copy()
+    for string in test_list:
+        count = string.count('1')
+        if count != number_expected:
+            new_test_list.remove(string)
+            print('removing string: ',string)
+    print('length of new test list:',len(new_test_list))
+    return new_test_list
+
+def make_full_strings(test_string,condition_records):
+    return 1
+
 def check_permutation(string):
     is_viable = False
     return is_viable
+
 '''
 Test input expectations:
 Number of arrangements per line should be 1, 4, 1, 1, 4, 10
@@ -41,13 +60,19 @@ for line_no,line in enumerate(content_list):
         else:
             print(f'Error - unexpected character encountered. Line {line_no} at character number {char_no}: \'{character}\'')
     total_springs = char_no + 1
-    print(f'total of {total_springs} springs. uncertain_springs: {uncertain_springs}.')
+    #print(f'total of {total_springs} springs. uncertain_springs: {uncertain_springs}.')
     for group in damaged_group_sizes:
         damaged_springs_total += int(group)
     springs_to_find = damaged_springs_total - damaged_springs_found
-    print(f'{damaged_springs_found} springs found out of a total of {damaged_springs_total} damaged springs. Remaining springs_to_find: {springs_to_find}')
-    combinations = math.comb(uncertain_springs,springs_to_find)
-    print('number of combinations = ',combinations)
+    #print(f'{damaged_springs_found} springs found out of a total of {damaged_springs_total} damaged springs. Remaining springs_to_find: {springs_to_find}')
+    print(f'looking for springs_to_find of {springs_to_find} amongst {uncertain_springs} uncertain_springs')
+    test_list = generate_combinations(uncertain_springs)
+    print(test_list)
+    test_list = remove_wrong_numbers(test_list,springs_to_find)
+    print('combinations predicted:',math.comb(uncertain_springs,springs_to_find))
+    #print(test_list)
+
+
 print('\nTotal possibilities: ',total_possibilities)
 '''
 I need to exclude damaged groups which are directly next to each other.
