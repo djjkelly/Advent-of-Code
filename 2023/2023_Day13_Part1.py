5887890d54046e6b249f5d6bcd8a516b90d8ec9a
Duncan Kelly <djjkelly01@gmail.com>
2024-05-02 15:49:06 +0100

    Correct answer found 2023_Day13_Part1

--- 2023/2023_Day13_Part1.py
+++ 2023/2023_Day13_Part1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #https://adventofcode.com/2023/day/13
 
-with open("2023/2023_Day13_test1_input.txt") as file_object:
+with open("2023/2023_Day13_input.txt") as file_object:
     file_content = file_object.readlines()
 
 sections = []
@@ -25,9 +25,6 @@ def check_candidates(section,horizontal_candidates):
         i2 = len(section) - candidate - 1
         iterations = min(i1,i2)
         print('evaluating candidate:',candidate,'. iterations:',iterations)
-        if iterations == 0:
-            print("-----------------------ZERO ITERATIONS REQUIRED")
-            return candidate
         candidate_viable = []
         for i in range(iterations):
             if section[candidate - i - 2] == section[candidate + i + 1]:
@@ -38,44 +35,35 @@ def check_candidates(section,horizontal_candidates):
             return candidate
 
 def find_mirror(section):
-    previous_string = ''
-    vert_bool = [None for l in range(len(section[0]))]
     horizontal_candidates,vertical_candidates = [],[]
+    previous_string = ''
     for index,string in enumerate(section):
         if previous_string != '':
-            if previous_string == string and index >0:
+            if previous_string == string:
                 number = int(index)
                 horizontal_candidates.append(number)
-            else:
-                previous_char = ''
-                for char_no,char in enumerate(string):
-                    if vert_bool[char_no] is not None:
-                        if char == previous_char:
-                            vert_bool[char_no] = True
-                        else:
-                            vert_bool[char_no] = False
-                    previous_char = char
         previous_string = string
-    for bool_no in range(len(vert_bool)):
-        if vert_bool[bool_no]:
-            number = int(bool_no)
-            vertical_candidates.append(number)
-    #print('horizontal_candidates:',horizontal_candidates,'vertical_candidates:', vertical_candidates)
-    horizontal_candidate = None
+    previous_string = ''
+    answer = None
     if horizontal_candidates != []:
-        horizontal_candidate = check_candidates(section,horizontal_candidates)
-        if horizontal_candidate:
-            answer = horizontal_candidate * 100
-            print('horizontal mirror found with',horizontal_candidate,'columns above.')
-    if vertical_candidates != []:
+        answer = check_candidates(section,horizontal_candidates)
+        if answer:
+            section_total = answer * 100
+            print('horizontal mirror found with',answer,'columns above.')
+    if not answer:
         transposed_section = [''.join(row[i] for row in section) for i in range(len(section[0]))]
+        for index,string in enumerate(transposed_section):
+            if previous_string != '':
+                if previous_string == string:
+                    number = int(index)
+                    vertical_candidates.append(number)
+            previous_string = string
+    if vertical_candidates != []:
         vertical_candidate = check_candidates(transposed_section,vertical_candidates)
         if vertical_candidate:
-            answer = vertical_candidate
-            print('vertical mirror found with',answer,'rows to the left.')
-    if vertical_candidates != [] and horizontal_candidates != [] and vertical_candidate and horizontal_candidate:
-        print('------------------------------------------------------------------------DOUBLE TROUBLE!')
-    return answer
+            section_total = vertical_candidate
+            print('vertical mirror found with',section_total,'rows to the left.')
+    return section_total
 
 grand_total = 0
 for i,section in enumerate(sections):
@@ -85,12 +73,13 @@ for i,section in enumerate(sections):
     print('section_total added:',section_total,'. current grand_total:',grand_total)
 print('Grand total:',grand_total)
 '''
-test_input should give a grand total of 405 (5 + 400)
+test1_input should give a grand total of 405 (5 + 400)
+test2_input should give a grand total of 1700
 
 18094 is not the right answer - too low.
 24655 is not the right answer - too low.
 32497 is not the right answer.
 28155 is not the right answer.
 29843 is not the right answer.
-
+29846 is the right answer!
 '''
