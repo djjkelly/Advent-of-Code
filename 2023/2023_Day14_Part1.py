c1b5ef591518f8755b26199f7f25b23737f91abe
Duncan Kelly <djjkelly01@gmail.com>
2024-05-02 18:06:59 +0100

    Day14 started

---
+++ 2023/2023_Day14_Part1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#https://adventofcode.com/2023/day/14
+
+with open("2023/2023_Day14_testinput.txt",'r') as file_object:
+    file_content = file_object.readlines()
+
+
+'''
+testinput should have a total load of 136
+'''
