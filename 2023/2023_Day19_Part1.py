0cee7ba2048e6cc3613f068b39645dec12f2c5e6
Duncan Kelly <djjkelly01@gmail.com>
2024-05-15 09:19:48 +0100

    Day19_Part1 solved

--- 2023/2023_Day19_Part1.py
+++ 2023/2023_Day19_Part1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #https://adventofcode.com/2023/day/19
 
-with open("2023/2023_Day19_testinput.txt",'r') as file_object:
+with open("2023/2023_Day19_input.txt",'r') as file_object:
     file_content = file_object.readlines()
     before_line_break = True
     workflows = {}
@@ -21,6 +21,7 @@ with open("2023/2023_Day19_testinput.txt",'r') as file_object:
             part_ratings.append(line_ratings)
 
 def process_action(action):
+    print(f'processing action {action}...')
     part_accepted = None
     next_workflow = None
     if action == 'R':
@@ -32,14 +33,15 @@ def process_action(action):
     return part_accepted,next_workflow
 
 def process_workflow(part,current_workflow):
+    print(f'processing workflow (part {part} and current_workflow {current_workflow})...')
     for rule in current_workflow:
         if ':' in rule:
             action = rule.split(':')[1]
             rule = rule.split(':')[0]
             category_to_consider = rule[0]
-            part_rating = part[category_to_consider]
+            part_rating = int(part[category_to_consider])
             conditional = rule[1]
-            number_to_check = rule[2:]
+            number_to_check = int(rule[2:])
             print('category_to_consider:',category_to_consider,'conditional:',conditional,'number_to_check:',number_to_check, 'action:', action)
             if conditional == '<':
                 if part_rating < number_to_check:
@@ -60,14 +62,18 @@ def process_workflow(part,current_workflow):
 
 accepted_parts = []
 for part in part_ratings:
+    print(f'new part! {part}')
     current_workflow_name = 'in'
     part_accepted = None
-    current_workflow = workflow_name[current_workflow_name]
-    while True:
-        current_workflow = workflow_name[current_workflow_name]
+    #current_workflow = workflow_name[current_workflow_name]
+    while part_accepted is None:
+        print('running while loop...')
+        current_workflow = workflows[current_workflow_name]
+        print(process_workflow(part,current_workflow))
         part_accepted,current_workflow_name = process_workflow(part,current_workflow)
         if part_accepted is True:
             accepted_parts.append(part)
+            print('TRUE! Appending part to list of accepted parts')
             break
         elif part_accepted is False:
             print('FALSE!')
@@ -84,5 +90,7 @@ print('total',total)
 '''
 testinput: total ratings = 19114
 individual ratings of 7540 for the part with x=787, 4623 for the part with x=2036, and 6951 for the part with x=2127
+correct testinput obtained
+correct answer obtained for Part1: 325952
 
 '''
