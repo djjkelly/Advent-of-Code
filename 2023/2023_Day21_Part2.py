4d9d6cc484838122d92ff116e91d7fb8aa91e8b2
Duncan Kelly <djjkelly01@gmail.com>
2024-05-30 19:58:16 +0100

    Day21_Part2 complete!
    I made a silly typing mistake which was making it all more difficult than it needed to be.
    I resorted back to inputting the odd or even status of the copy in question into the function, to get this over the line.
    However I'm not sure if this is strictly necessary

--- 2023/2023_Day21_Part2.py
+++ 2023/2023_Day21_Part2.py
@@ -19,8 +19,7 @@ for line_no,line in enumerate(file_content):
     input_list.append(line)
 vertical_length = line_no
 horizontal_length = char_no
-print('vertical_length:',vertical_length,'horizontal_length:',horizontal_length)
-print(f'start point. line: {start_line} char: {start_char}')
+total = 0
 '''
 The starting point (0,0) starts off even, but after an odd number of steps it will not be one of the possibilities.
 The numbers of lines and columns in the input data are both odd (131,131)
@@ -63,15 +62,15 @@ directions = {
 
 number_of_steps = 26501365 # as per problem statement
 copy_count = int((number_of_steps - start_char)/(horizontal_length + 1)) # this is the number of copies
-print('copy_count:',copy_count)
 steps_to_fully_fill = 130 # this will always be enough steps to fill a grid from the midpoint (= height//2 + width//2).
 
-def fill_copy(start_line, start_char, steps):
-    if steps % 2 == 0:
-        even_or_odd = 'even'
+def fill_copy(start_line, start_char, even_or_odd_copy, steps):
+    if (start_line % 2 == 0) == (start_char % 2 == 0):
+        even_or_odd_start = 'even'
     else:
-        even_or_odd = 'odd'
-    squares_to_evaluate = { (start_line, start_char): even_or_odd }
+        even_or_odd_start = 'odd'
+
+    squares_to_evaluate = { (start_line, start_char): even_or_odd_start }
     squares_evaluated = {}  # Use a dictionary to track all evaluated coordinates and their statuses
     for step_count in range(steps):
         current_squares_to_evaluate = squares_to_evaluate.copy()
@@ -94,18 +93,21 @@ def fill_copy(start_line, start_char, steps):
         #print('step finished!')
     plots_per_filled_copy = 0
     for coordinates, status in squares_evaluated.items():
-        if status == 'even':
+        if status == even_or_odd_copy:
             plots_per_filled_copy += 1
     return plots_per_filled_copy
 
-test_even = fill_copy(65,65,64)
-print('should be 3699. test_even:',test_even) # should still be 3699 - and it is!
+test_even = fill_copy(65,65,'even',64)
 if test_even == 3699:
-    print('TEST PASSED! 3699 condition met\n')
+    print('TEST PASSED! 3699 condition met')
 
 # this part calculates the total reachable plots for copies which are completely filled in
-count_for_odd_copies = fill_copy(start_line,start_char,131)
-counts_for_even_copies = fill_copy(start_line,start_char,130) # 130 is the amount of moves needed to fill the end, not 131! (130 and 132 give same answer, meaning it's full)
+count_for_odd_copies = fill_copy(start_line,start_char,'odd',130)
+counts_for_even_copies = fill_copy(start_line,start_char,'even',130) # 130 is the amount of moves needed to fill the end, not 131! (130 and 132 give same answer, meaning it's full)
+
+# an 'odd copy' is one where the 65,65 point will be counted on an even number of steps (i.e. it matches the original map).
+# an 'odd square' which will be counted on an odd number of steps (like 65,66 in the original map, or 65,65 in the 4 next (even) copies).
+# an 'even copy' is one where the 65,65 point will be counted on an odd number of steps
 
 number_of_odd_filled_copies = (copy_count - 1) ** 2
 number_of_even_filled_copies = (copy_count) ** 2
@@ -115,35 +117,35 @@ print('even per-copy:',count_for_odd_copies,'odd per-copy:',counts_for_even_copi
 print('filled_copies_total:',filled_copies_total)
 
 # this part calculates the partially filled copies at the farthest right, left, up, down points
-right_point = fill_copy(65,0,130) # the last input to fill_copy is 131 as the steps go right to the end of the last copy
-left_point = fill_copy(65,130,130)
-top_point = fill_copy(0,65,130)
-bottom_point = fill_copy(130,65,130)
+right_point = fill_copy(65,0,'odd',130) # the last input to fill_copy is 131 as the steps go right to the end of the last copy
+left_point = fill_copy(65,130,'odd',130)
+top_point = fill_copy(0,65,'odd',130)
+bottom_point = fill_copy(130,65,'odd',130)
 
 print('right_point:',right_point,'left_point:',left_point,'top_point:',top_point,'bottom_point:',bottom_point)
 total = filled_copies_total + right_point + left_point + top_point + bottom_point
 print('intermediate total:',total)
 
 # calculation for odd and even copies on the diagonals:
-top_right_big_edge = fill_copy(130,0,195) # big edges should be odd
-top_right_small_edge = fill_copy(130,0,65) # small edges should be even
-top_left_big_edge = fill_copy(130,130,195)
-top_left_small_edge = fill_copy(130,130,65)
-bottom_right_big_edge = fill_copy(0,0,195)
-bottom_right_small_edge = fill_copy(0,0,65)
-bottom_left_big_edge = fill_copy(0,130,195)
-bottom_left_small_edge = fill_copy(0,130,65)
-print(f'top_right_even_edge: {top_right_big_edge},top_right_odd_edge: {top_right_small_edge},  top_left_big_edge: {top_left_big_edge}, top_left_small_edge: {top_left_small_edge}')
+top_right_big_edge = fill_copy(130,0,'odd',195) # big edges should be odd
+top_right_small_edge = fill_copy(130,0,'even',65) # small edges should be even
+top_left_big_edge = fill_copy(130,130,'odd',195)
+top_left_small_edge = fill_copy(130,130,'even',65)
+bottom_right_big_edge = fill_copy(0,0,'odd',195)
+bottom_right_small_edge = fill_copy(0,0,'even',65)
+bottom_left_big_edge = fill_copy(0,130,'odd',195)
+bottom_left_small_edge = fill_copy(0,130,'even',65)
+print(f'top_right_big_edge: {top_right_big_edge}, top_right_odd_edge: {top_right_small_edge},  top_left_big_edge: {top_left_big_edge}, top_left_small_edge: {top_left_small_edge}')
 print(f'bottom_right_big_edge: {bottom_right_big_edge}, bottom_right_small_edge: {bottom_right_small_edge}, bottom_left_big_edge: {bottom_left_big_edge}, bottom_left_small_edge: {bottom_left_small_edge}')
 
 big_diagonals = (top_right_big_edge + top_left_big_edge + bottom_right_big_edge + bottom_left_big_edge) * (copy_count - 1)
-small_diagonals = (top_right_small_edge + top_right_small_edge + bottom_right_small_edge + bottom_left_small_edge) * (copy_count)
+small_diagonals = (top_right_small_edge + top_left_small_edge + bottom_right_small_edge + bottom_left_small_edge) * (copy_count)
 total += (big_diagonals + small_diagonals)
 
 test_dictionary = {
     '2023/2023_Day21_input.txt':
     {'attempts':(613391300444549,613388299113968,613391342118089,613391289520349,613391299230782,613391321079128,613391268076632,613391289925448,613391289924978),
-    'low':602668880128698,'high':613391353042289,'answer':None},
+    'low':602668880128698,'high':613391353042289,'answer':613391294577878},
 }
 print('total:',total)
 
