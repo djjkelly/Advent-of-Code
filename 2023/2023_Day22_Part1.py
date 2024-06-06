3cd4f8ddb3b67004449ea49c5c53b46fbb226c61
Duncan Kelly <djjkelly01@gmail.com>
2024-06-06 17:27:14 +0100

    Day22_Part1 solved
    I had to fix the return_min_z_heights function which was broken, but working fine in the specific example of the test data.
    Bricks were not all landing properly based on previous_bricks.

--- 2023/2023_Day22_Part1.py
+++ 2023/2023_Day22_Part1.py
@@ -52,23 +52,15 @@ print('test for intersects (should be true):',intersects(testb1,testb2))
 def return_min_z_heights():
     z_heights = {}
     recent_bricks = []
-    for index,brick in enumerate(ordered_bricks_list):
-        if index == 0:
-            z_heights[brick] = 1
-            recent_bricks.insert(0,brick)
-            continue
-        previous_brick = ordered_bricks_list[index - 1]
-        if intersects(previous_brick,brick):
-            z_heights[brick] = z_heights[previous_brick] + 1
-            recent_bricks.insert(0,brick)
-        else:
-            for previous_brick in recent_bricks:
-                if intersects(previous_brick,brick):
-                    z_heights[brick] = z_heights[previous_brick] + 1
-                    recent_bricks.insert(0,brick)
-                    break
-                else:
-                    z_heights[brick] = 1
+    for brick in ordered_bricks_list:
+        min_z = 1 # this still initialises the first brick in the ordered_bricks_list, which will be 1 anyway
+        for previous_brick in recent_bricks:
+            if intersects(previous_brick, brick):
+                potential_z = z_heights[previous_brick] + (previous_brick[1][2] - previous_brick[0][2]) + 1
+                if potential_z > min_z:
+                    min_z = potential_z
+        z_heights[brick] = min_z
+        recent_bricks.append(brick)
     return z_heights
 min_z_heights = return_min_z_heights()
 
@@ -87,7 +79,7 @@ def drop_bricks_to_final_z_positions(ordered_bricks_list,min_z_heights):
             new_z_max = new_z_min
         new_brick = ((brick[0][0],brick[0][1],new_z_min),(brick[1][0],brick[1][1],new_z_max))
         fallen_bricks_list.append(new_brick)
-    fallen_bricks_list = sorted(fallen_bricks_list, key=lambda x: int(x[0][2])) # sorting by z again - is this necessary? Do we want sorted by max or min?
+    fallen_bricks_list = sorted(fallen_bricks_list, key=lambda x: int(x[0][2]))
     return fallen_bricks_list
 fallen_bricks_list = drop_bricks_to_final_z_positions(ordered_bricks_list,min_z_heights)
 
@@ -104,52 +96,52 @@ def is_lower_support_of_higher(lower_brick,higher_brick): # accepts the original
         if higher_brick[0][2] == lower_brick[1][2] + 1:
             return True
     return False
-#print('test_is_support (should be True): ',is_lower_support_of_higher(fallen_bricks_list[1],fallen_bricks_list[3]))
+#print('test_is_support (should be True for testinput): ',is_lower_support_of_higher(fallen_bricks_list[1],fallen_bricks_list[3]))
 
 '''
-If a brick is not supporting another brick, it can be disintegrated.
+If a brick is not supporting another brick, it can be disintegrated (non-support bricks).
 When a brick is supporting another brick, it can only be disintegrated if all the bricks it's supporting are supported by at least one other brick.
 '''
 
-def count_blocks_supporting_each_brick(fallen_bricks_list):
-    higher_bricks_being_supported_by_each_brick = {}
-    number_of_bricks_not_supporting_other_bricks = 0
-    for index,brick in enumerate(fallen_bricks_list):
+def count_disintegrable_bricks(fallen_bricks_list):
+    lower_to_higher_supports_dictionary = {}
+    higher_brick_support_counts = {}
+    
+    non_support_brick_count = 0
+    support_bricks = []
+    disintegrable_supports,critical_count = 0,0
+    for index,lower_brick in enumerate(fallen_bricks_list):
         higher_bricks_supported = []
+        is_lower_brick_support = False
         for higher_brick in fallen_bricks_list[index+1:]:
-            if is_lower_support_of_higher(lower_brick = brick, higher_brick = higher_brick):
-                #print('True for lower brick',brick,'higher brick ',check_brick)
+            if is_lower_support_of_higher(lower_brick,higher_brick):
                 higher_bricks_supported.append(higher_brick)
-        if higher_bricks_supported == []:
-            number_of_bricks_not_supporting_other_bricks += 1
+                is_lower_brick_support = True
+                if higher_brick in higher_brick_support_counts:
+                    higher_brick_support_counts[higher_brick] += 1
+                else:
+                    higher_brick_support_counts[higher_brick] = 1
+        if is_lower_brick_support:
+            support_bricks.append(lower_brick)
+            lower_to_higher_supports_dictionary[lower_brick] = higher_bricks_supported
         else:
-            higher_bricks_being_supported_by_each_brick[brick] = higher_bricks_supported
-    return higher_bricks_being_supported_by_each_brick , number_of_bricks_not_supporting_other_bricks
-higher_bricks_being_supported_by_each_brick, total = count_blocks_supporting_each_brick(fallen_bricks_list)
+            non_support_brick_count += 1
+    print('number of support bricks:',len(support_bricks))
+    for support_brick in support_bricks:
+        if all(higher_brick_support_counts[higher_brick]>1 for higher_brick in lower_to_higher_supports_dictionary[support_brick]):
+            disintegrable_supports += 1
+        else:
+            critical_count += 1
 
-'''
-Now I need to consider the bricks which are supporting other bricks
-'''
-
-def iterate_supporting_counts(higher_bricks_being_supported):
-    count = 0
-    values = higher_bricks_being_supported.values()
-    # aims to count the number of times each higher_brick appears in the values()
-    for higher_bricks in values:
-        can_be_disintegrated = True
-        for higher_brick in higher_bricks:
-            if sum(sublist.count(higher_brick) for sublist in values) <= 1:
-                can_be_disintegrated = False
-        if can_be_disintegrated:
-            count += 1
-    return count
-total += iterate_supporting_counts(higher_bricks_being_supported_by_each_brick)
+    print(f'end of function. count:{disintegrable_supports}. critical_count:{critical_count}')
+    return non_support_brick_count + disintegrable_supports
+total = count_disintegrable_bricks(fallen_bricks_list)
 
 print('total:',total)
 test_dictionary = {
     '2023_Day22_input':
     {'attempts':(None),
-    'low':464,'high':670,'answer':None},
+    'low':464,'high':670,'answer':492},
     '2023_Day22_testinput':
     {'answer':5},
 }
