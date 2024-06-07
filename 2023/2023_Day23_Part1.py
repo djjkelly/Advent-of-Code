e2a0489c5d03f7a7c6485c9f1245df404ec2c48a
Duncan Kelly <djjkelly01@gmail.com>
2024-06-07 18:42:47 +0100

    Initial progress on Day23

---
+++ 2023/2023_Day23_Part1.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+#https://adventofcode.com/2023/day/23
+
+folder = '2023/'
+filename = '2023_Day23_testinput'
+extension = '.txt'
+full_path = folder + filename + extension
+with open(full_path,'r') as file_object:
+    file_content = file_object.readlines()
+total = 0
+input_list = []
+for line in file_content:
+    line = line.strip()
+    input_list.append(line)
+vertical_length = len(input_list)
+horizontal_length = len(input_list[0])
+start_v = 0
+start_h = 1
+end_v = vertical_length - 1
+end_h = horizontal_length - 2
+start_direction = 'down'
+# these simplifications are true for both test and real input.
+
+directions = {
+    'down':(1,0),
+    'right':(0,1),
+    'up':(-1,0),
+    'left':(0,-1)
+}
+forced_directions = {
+    'v':'down',
+    '>':'right',
+    '^':'up',
+    '<':'left',
+}
+
+def is_in_bounds(new_v,new_h):
+    return 1 <= new_v < vertical_length - 1 and 1 <= new_h < horizontal_length - 1 # we never have to explore the outer edge.
+
+'''
+Any section of path which does not contain a fork can have its length stored in memory for later reuse.
+If the start and end coordinates and direction are recorded, this memoised path can be accessed from both sides.
+
+It looks like there aren't any > symbols going straight into the forest.
+Also, single sections don't seem to have 
+Rather the slippery slopes seem to act as 'gates' at both ends of each section.
+It looks like, if I can't get in one end, then I can't get out the other either.
+If I can get in one end of the section, then I can get out the other.
+The sections basically all start/end with a '<', '>', 'v', '^' symbol.
+This also means that all "sections" are one-way.
+'''
+
+def explore_section(start_state):
+    v,h,direction = start_state
+    section_length = 0
+    section_evaluation_ongoing = True
+    while section_evaluation_ongoing:
+        section_length += 1
+        for new_direction,(dv,dh) in directions.items():
+            if (-dv,-dh) == directions[direction]:
+                continue # can't go back on ourselves
+            new_v, new_h = v + dv, h + dh
+            if not is_in_bounds(new_v,new_h):
+                continue # can't go out of bounds
+            new_character = input_list[new_v][new_h]
+            if new_character == '#': # ignore forest
+                continue
+            if new_character in forced_directions: # has reached the end of the section!
+                section_evaluation_ongoing = False
+                section_length += 1
+                v,h,direction = new_v,new_h,new_direction
+                break
+            if new_character == '.':
+                v,h,direction = new_v,new_h,new_direction
+                break
+
+
+    end_state = (new_v,new_h,new_direction)
+    return end_state, section_length
+end_state, section_length = explore_section((0,1,'down')) # aiming for 18,19
+print('end_state',end_state,'section_length',section_length)
+
+def explore_paths():
+    steps_required = 0 # consider setting this to 1
+    state = (start_h,start_v,start_direction)
+    return steps_required
+total = explore_paths()
+
+print('total:',total)
+test_dictionary = {
+    '2023_Day23_input':
+    {'attempts':(None),
+    'low':None,'high':None,'answer':None},
+    '2023_Day23_testinput':
+    {'answer':94},
+}
+
+from testmodule import test_function
+test_function(test_dictionary,filename,total)
+'''
+
+'''
