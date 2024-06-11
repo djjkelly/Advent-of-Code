fee06d521cd9e5e2c7c2b4d7fdf200a1645b4339
Duncan Kelly <djjkelly01@gmail.com>
2024-06-11 11:58:32 +0100

    Simplified directions

--- 2023/2023_Day23_Part1.py
+++ 2023/2023_Day23_Part1.py
@@ -18,20 +18,14 @@ start_v = 0
 start_h = 1
 end_v = vertical_length - 2
 end_h = horizontal_length - 2
-start_direction = 'down'
+start_direction = 'v'
 # these simplifications are true for both test and real input.
 
 directions = {
-    'down':(1,0),
-    'right':(0,1),
-    'up':(-1,0),
-    'left':(0,-1)
-}
-forced_directions = {
-    'v':'down',
-    '>':'right',
-    '^':'up',
-    '<':'left',
+    'v':(1,0),
+    '>':(0,1),
+    '^':(-1,0),
+    '<':(0,-1)
 }
 
 def is_in_bounds(new_v,new_h):
@@ -53,15 +47,13 @@ This also means that all "sections" are one-way.
 def explore_section(start_state):
     v,h,direction = start_state
     section_length = 0
-    section_evaluation_ongoing = True
-    while section_evaluation_ongoing: # each iteration represents a step along the path section
+    while True: # each iteration represents a step along the path section
         section_length += 1
         for new_direction,(dv,dh) in directions.items():
             if (v,h) == (end_v,end_h): # end of puzzle (one move away from end)
                 section_length += 1
-                section_evaluation_ongoing = False
                 print('reached end of puzzle!')
-                end_state = (None,None,new_direction)
+                end_state = (None,None,None)
                 return end_state, section_length
             if (-dv,-dh) == directions[direction]:
                 continue # can't go back on ourselves
@@ -74,12 +66,10 @@ def explore_section(start_state):
             if new_character == '.':
                 v,h,direction = new_v,new_h,new_direction
                 break
-            if new_character in forced_directions: # has reached the end of the section!
-                section_evaluation_ongoing = False
+            if new_character in directions: # has reached the end of the section!
                 section_length += 1
-                forced_direction = forced_directions[new_character]
-                (fdv,fdh) = directions[forced_direction]
-                end_state = (new_v + fdv, new_h + fdh,forced_direction)
+                (fdv,fdh) = directions[new_character]
+                end_state = (new_v + fdv, new_h + fdh,new_character)
                 return end_state, section_length
 #end_state, section_length = explore_section((0,1,'down')) # aiming for 18,19
 #print('end_state',end_state,'section_length',section_length)
@@ -92,8 +82,6 @@ def explore_all_sections(input_list):
     while queue:
         start_state = queue.pop(0)
         end_state,section_length = explore_section(start_state)
-        if start_state == initial_state:
-            initial_end_state = end_state
         if end_state in section_lengths:
             print('end state dupicate encountered - check this!')
         else:
@@ -106,7 +94,7 @@ def explore_all_sections(input_list):
         v,h = end_v, end_h
         print(f'section ends at ({end_v},{end_h}), new state at ({v},{h})')
         # find all viable directions for the next sections to be added to the queue
-        for new_direction, (dv,dh) in directions.items():
+        for (dv,dh) in directions.values():
             if (dv,dh) == (-end_dv,-end_dh):
                 continue
             new_v, new_h = v + dv,h + dh
@@ -115,13 +103,12 @@ def explore_all_sections(input_list):
             new_character = input_list[new_v][new_h]
             if new_character == '#':
                 continue
-            if new_character in forced_directions:
-                if forced_directions[new_character] == new_direction:
-                    print('new section found! adding to queue')
-                    new_state = (new_v,new_h,new_direction)
-                    if new_state not in queue:
-                        queue.append(new_state)
+            if new_character in directions:
+                new_state = (new_v,new_h,new_character)
+                if new_state not in queue:
+                    queue.append(new_state)
     return section_lengths, start_end_mapping
+
 section_lengths, start_end_mapping = explore_all_sections(input_list)
 # seems ok so far. For testinput it identifies 12 sections and the new states seem to be at the correct points
 for section,length in section_lengths.items():
@@ -135,9 +122,9 @@ def find_longest_path_length(section_lengths, start_end_mapping):
     while paths_to_explore:
         current_path = paths_to_explore.pop(0)
         v, h, direction = current_path[0]
-        (dv,dh) = directions[direction]
         current_total = current_path[1]
         if v is not None and h is not None:
+            (dv,dh) = directions[direction]
             for new_direction,(ndv,ndh) in directions.items():
                 if (dv,dh) == (-ndv,-ndh):
                     'continue'
@@ -146,9 +133,9 @@ def find_longest_path_length(section_lengths, start_end_mapping):
                     next_end_status = start_end_mapping[next_status]
                     paths_to_explore.append((next_end_status,current_total + section_lengths[next_end_status]))
         else:
-            total = current_total + section_lengths[(None,None,'down')]
+            total = current_total + section_lengths[(None,None,None)]
             list_of_totals.append(total)
-    print(f'list_of_totals {list_of_totals} - should be ( 90, 82,  74, 82, 86 and 94)')
+    print(f'list_of_totals {list_of_totals} - should be ( 90, 82,  74, 82, 86, 94 )')
     max_total = 0
     for total in list_of_totals:
         if total > max_total:
