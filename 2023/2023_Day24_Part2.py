6d60c2560ed8611993e7d16bcae6d44261511955
Duncan Kelly <djjkelly01@gmail.com>
2024-06-17 12:39:00 +0100

    Day24 Part2 solved using sympy

--- 2023/2023_Day24_Part2.py
+++ 2023/2023_Day24_Part2.py
@@ -21,35 +21,39 @@ for line in file_content:
     velocities_list.append(velocities)
 
 '''
+I think we only need to look at 3 points to figure out the starting position and velocity.
+Start positions x3 and velocities x3 need to be taken into account.
+t represents the time of the actual impact (for each hailstone)
+rock_start_x = px + (t * (rock_start_vx - vx)      or      t = ( rock_start_x - px ) / (rock_start_vx - vx)
+rock_start_y = py + (t * (rock_start_vy - vy)      or      t = ( rock_start_y - py ) / (rock_start_vy - vy)
+rock_start_z = pz + (t * (rock_start_vz - vz)      or      t = ( rock_start_z - pz ) / ( rock_start_vz - vz)
+
+eliminate t as we are calculating start values...
+( rock_start_x - px ) / ( rock_start_vx - vx ) = ( y - py ) / ( rock_start_vy - vy )
+( rock_start_y - py ) / ( rock_start_vy - vy ) = ( z - pz ) / ( rock_start_vz - vz )
+
+rewritten...
+( rock_start_vy - vy ) * ( rock_start_x - px ) = ( rock_start_vx - vx ) * ( rock_start_y - py )
+( rock_start_vz - vz ) * ( rock_start_y - py ) =  ( rock_start_vy - vy) * ( rock_start_z - pz )
+or with a comma instead of '='...
+( rock_start_vy - vy ) * ( rock_start_x - px ), ( rock_start_vx - vx ) * ( rock_start_y - py )
+( rock_start_vz - vz ) * ( rock_start_y - py ),  ( rock_start_vy - vy) * ( rock_start_z - pz )
 
 '''
 def count_intersections(positions_list,velocities_list):
-    intersections_count = 0
-    for i in range(len(positions_list)):
-        px1,py1,_ = positions_list[i]
-        vx1,vy1,_ = velocities_list[i]
-        g1 = vy1/vx1
-        for j in range(i + 1,len(positions_list)):
-            px2,py2,_ = positions_list[j]
-            vx2,vy2,_ = velocities_list[j]
-            g2 = vy2/vx2
-            print(f'testing {positions_list[i]}{velocities_list[i]}, gradient {g1},     against {positions_list[j]}{velocities_list[j]}, gradient {g2}')
-            if g1 == g2:
-                print('lines parralel, skipping this comparison')
-                continue # not added to count
-            t2 = ( ( px1 * vy1 ) - ( px2 * vy1 ) + ( py2 * vx1 ) - ( py1 * vx1 ) ) / ( ( vx2 * vy1 ) - ( vy2 * vx1 ) )
-            ix = px2 + (t2 * vx2)
-            iy = py2 + (t2 * vy2)
-            t1 = ( ix - px1 ) / vx1
-            ti = min(t1,t2) # time at first intersection
-            if 200000000000000 <= ix <= 400000000000000 and 200000000000000 <= iy <= 400000000000000:
-                if ti > 0:
-                    intersections_count +=1
-                else:
-                    print('happened in the past!')
-            else:
-                print('outside grid!')
-    return intersections_count
+    equations = []
+    rock_start_x,rock_start_y,rock_start_z,rock_start_vx,rock_start_vy,rock_start_vz = symbols('rock_start_x, rock_start_y, rock_start_z, rock_start_vx, rock_start_vy, rock_start_vz')
+    for i in range(4): # 4 works but 3 doesn't
+        px,py,pz = positions_list[i]
+        vx,vy,vz = velocities_list[i]
+        equations.extend([Eq(( rock_start_vy - vy ) * ( rock_start_x - px ), ( rock_start_vx - vx ) * ( rock_start_y - py )), Eq(( rock_start_vz - vz ) * ( rock_start_y - py ),  ( rock_start_vy - vy) * ( rock_start_z - pz )) ])
+    for equation in equations:
+        print(equation)
+    solution = solve(equations,(rock_start_x,rock_start_y,rock_start_z,rock_start_vx,rock_start_vy,rock_start_vz))
+    print(solution)
+    rock_start_x, rock_start_y, rock_start_z = solution[0][0],solution[0][1],solution[0][2]
+    start_pos_multiple = rock_start_x + rock_start_y + rock_start_z
+    return start_pos_multiple
 total = count_intersections(positions_list,velocities_list)
 
 
@@ -57,7 +61,7 @@ print('total:',total)
 test_dictionary = {
     '2023_Day24_input':
     {'attempts':(None),
-    'low':None,'high':None,'answer':None},
+    'low':None,'high':None,'answer':566373506408017},
     '2023_Day24_testinput':
     {'answer':47},
 }
--- 2023/sympy_practice.py
+++ 2023/sympy_practice.py
@@ -4,5 +4,6 @@ x = symbols('x')
 y = symbols('y')
 
 equation = Eq(x**3 - 4*x**2 + 13*x - 4, 42*y)
-answer = solve([equation], x)
+equations = [Eq(x**2, 9),Eq(x**3, -27), Eq(y, 3 * x**2)]
+answer = solve(equations, (x,y))
 print('answer:',answer)
