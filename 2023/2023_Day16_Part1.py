1b0d61e433cd59ca556aa0e486ade92a392764b2
Duncan Kelly <djjkelly01@gmail.com>
2024-07-05 14:48:03 +0100

    Initial progress on improved solution

--- 2023/2023_Day16_Part1.py
+++ 2023/2023_Day16_Part1.py
@@ -39,85 +39,35 @@ when hit side on, the beam is split into both perpendicular directions.
 LIGHT BEAM enters from left, in top-left corner.
 '''
 
-def move_to_next(line_no,char_no,direction):
-    new_line_no,new_char_no = line_no,char_no
-    if direction == 'right':
-        new_char_no = char_no + 1
-    elif direction == 'down':
-        new_line_no = line_no + 1
-    elif direction == 'left':
-        new_char_no = char_no - 1
-    elif direction == 'up':
-        new_line_no = line_no - 1
-    return(new_line_no,new_char_no)
+energised_tiles = set()
+directions = {
+        'right': (0, 1), 'down': (1, 0), 'up': (-1, 0), 'left': (0, -1)
+    }
+mirror_reflections = {
+        '/': {'right': 'up', 'up': 'right', 'left': 'down', 'down': 'left'},
+        '\\': {'right': 'down', 'down': 'right', 'left': 'up', 'up': 'left'}
+    }
+splitters = {
 
-def explore_branch(line_no,char_no,direction):
+}
+
+list_of_branches_to_explore = [(0,0,'right')] # initial direction 'right' upon beam entering array
+seen_beams = set()
+while list_of_branches_to_explore != []:
+    current_beam = list_of_branches_to_explore.pop(0)
+    if current_beam not in seen_beams:
+        seen_beams.append(current_beam)
     branch_continues = True
     while branch_continues:
-        if char_no != -1:
-            energisation_map[line_no] = energisation_map[line_no][:char_no] + '#' + energisation_map[line_no][char_no+1:]
-        direction_checked = {'line_no':line_no,'char_no':char_no,'direction':direction}
-        if direction_checked not in directions_checked:
-            directions_checked.append(direction_checked)
-        else:
+        (r,c,direction) = current_beam
+        energisation_map[r][c] = '#'
+        (dr,dc) = directions[direction]
+        nr , nc = r + dr , c + dc
+        if nr < 0 or nc < 0 or r == len(input_list) or c == len(input_list):
             branch_continues = False
-            print(f'end of branch at line due to repeated direction: {line_no} char: {char_no}')
+            print(f'end of branch at line: {r} char: {c}')
             break
-        line_no,char_no = move_to_next(line_no,char_no,direction) # reassigning line_no and char_no to new values
-        if line_no < 0 or char_no < 0 or line_no == len(input_list) or char_no == len(input_list):
-            branch_continues = False
-            print(f'end of branch at line: {line_no} char: {char_no}')
-            break
-        if input_list[line_no][char_no] == '.':
-            continue
-        # setting next direction...
-        if input_list[line_no][char_no] == '/':
-            if direction == 'right':
-                direction = 'up'
-            elif direction == 'down':
-                direction = 'left'
-            elif direction == 'left':
-                direction = 'down'
-            elif direction == 'up':
-                direction = 'right'
-        elif input_list[line_no][char_no] == '\\':
-            if direction == 'right':
-                direction = 'down'
-            elif direction == 'down':
-                direction = 'right'
-            elif direction == 'left':
-                direction = 'up'
-            elif direction == 'up':
-                direction = 'left'
-        elif input_list[line_no][char_no] == '-':
-            if direction == 'right' or direction == 'left':
-                continue
-            if direction == 'down' or direction == 'up':
-                direction = 'left'
-                if {'line_no':line_no,'char_no':char_no,'direction':'right'} not in list_of_historic_branches_explored:
-                    if {'line_no':line_no,'char_no':char_no,'direction':'right'} not in list_of_branches_to_explore:
-                        list_of_branches_to_explore.append({'line_no':line_no,'char_no':char_no,'direction':'right'})
-        elif input_list[line_no][char_no] == '|':
-            if direction == 'down' or direction == 'up':
-                continue
-            if direction == 'right' or direction == 'left':
-                direction = 'up'
-                if {'line_no':line_no,'char_no':char_no,'direction':'down'} not in list_of_historic_branches_explored:
-                    if {'line_no':line_no,'char_no':char_no,'direction':'down'} not in list_of_branches_to_explore:
-                        list_of_branches_to_explore.append({'line_no':line_no,'char_no':char_no,'direction':'down'})
-
-list_of_branches_to_explore = [{'line_no':0,'char_no':-1,'direction':'right'}] # initial direction 'right' upon beam entering array
-list_of_historic_branches_explored = []
-directions_checked = []
-branch_continues = True
-while list_of_branches_to_explore != []:
-    line_no = list_of_branches_to_explore[0]['line_no']
-    char_no = list_of_branches_to_explore[0]['char_no']
-    direction = list_of_branches_to_explore[0]['direction']
-    historic_branch = list_of_branches_to_explore.pop(0)
-    if historic_branch not in list_of_historic_branches_explored:
-        list_of_historic_branches_explored.append(historic_branch)
-    explore_branch(line_no,char_no,direction)
+        current_tile = input_list[r][c]
 
 energised_tiles_no = 0
 for line in energisation_map:
